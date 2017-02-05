5120620094fdad1ea00352894b8c24cb255fbf5b
Stefan <mail.ipv4v6+gh@gmail.com>
2017-02-05 13:00:36 +0100

    add basic style

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ WORKDIR=`pwd`
 DESTDIR=~/DESTDIR
 PAGEDIR=${WORKDIR}/pages
 TMPLDIR=${WORKDIR}/templates
+CSSDIR=styles
+CSSFILE=ipv4v6.css
 
 PAGELIST="index"
 
@@ -15,5 +17,7 @@ mkdir -p ${DESTDIR}
 
 for PAGE in ${PAGELIST} ; do
 	pandoc -f markdown -t html5 --data-dir ${WORKDIR} --template ipv4v6 \
-	-s -S -o ${DESTDIR}/${PAGE}.html ${PAGEDIR}/${PAGE}.md
+	--css ${CSSDIR}/${CSSFILE} -s -S \
+	-o ${DESTDIR}/${PAGE}.html ${PAGEDIR}/${PAGE}.md
 done
+cp -a ${CSSDIR} ${DESTDIR}
